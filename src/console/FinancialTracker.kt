5dd2def8e633db6af1dcca8ca6c79b9446438c89
Thoraya Hamdy <thoriahamdy99@gmail.com>
2025-04-09 00:15:24 +0200

    - add default implementation for get transactions by month to use it while making test cases.

--- src/console/FinancialTracker.kt
+++ src/console/FinancialTracker.kt
@@ -3,6 +3,8 @@ package src.console
 import src.model.Transaction
 import src.storage.IFinancialTrackerStorage
 
-class FinancialTracker(val iFinancialTrackerStorage: IFinancialTrackerStorage) {
-
+class FinancialTracker(val financialTrackerStorage: IFinancialTrackerStorage) {
+    fun getTransactionByMonth(month: String): List<Transaction>? {
+        return null
+    }
 }

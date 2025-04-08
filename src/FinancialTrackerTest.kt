1d2af9e1e5e6f7f10be94568fabefe7a54879f69
itsahmedsalah <158590164+itsahmedsalah@users.noreply.github.com>
2025-04-09 00:16:27 +0200

    added testcases for get transaction by id function.

--- src/FinancialTrackerTest.kt
+++ src/FinancialTrackerTest.kt
@@ -1,9 +1,41 @@
 package src
 
+import src.model.Transaction
+import src.storage.MemoryFinancialTrackerStorage
+
 
 fun main() {
     print("hello")
+
+    // start region for view transaction
+    // get by id
+
+    val memoryFinancialTrackerStorage = MemoryFinancialTrackerStorage()
+    check(
+        name = "when user send valid id, return true",
+        result = memoryFinancialTrackerStorage.getTransactionById(2) != null,
+        expectedResult = true
+    )
+    check(
+        name = "when user send out of range id, return false",
+        result = memoryFinancialTrackerStorage.getTransactionById(100) != null,
+        expectedResult = false
+    )
+
+    check(
+        name = "when user send out of range and negative id, return false",
+        result = memoryFinancialTrackerStorage.getTransactionById(-1) != null,
+        expectedResult = false
+    )
+    check(
+        name = "when user send zero, return false",
+        result = memoryFinancialTrackerStorage.getTransactionById(0) != null,
+        expectedResult = false
+    )
+
+    // end region for view transaction
 }
+
 
 fun check(name: String, result: Boolean, expectedResult: Boolean) {
     if (result == expectedResult) {

db0fe77e1809d74b33d52e28938f598f6d098b09
Thoraya Hamdy <thoriahamdy99@gmail.com>
2025-04-10 00:37:38 +0200

    - update test cases for get transaction by month.

--- src/FinancialTrackerTest.kt
+++ src/FinancialTrackerTest.kt
@@ -22,107 +22,72 @@ fun testGetTransactionMonthlyReport() {
     val financialTracker = FinancialTracker(financialTrackerStorage)
 
     /** Invalid test cases */
+
     check(
-        name = "When have negative number like -1 it should return null",
-        result = financialTracker.getTransactionByMonth("-1") == null,
-        expectedResult = true
-    )
-    check(
-        name = "When have  number < 1 like -13 it should return null",
+        name = "When month < 1 it should return null",
         result = financialTracker.getTransactionByMonth("-13") == null,
         expectedResult = true
     )
+
     check(
-        name = "When have number == 0 it should return null",
-        result = financialTracker.getTransactionByMonth("0") == null,
-        expectedResult = true
-    )
-    check(
-        name = "When have  number > 12 it should return null",
+        name = "When month > 12 it should return null",
         result = financialTracker.getTransactionByMonth("13") == null,
         expectedResult = true
     )
     check(
-        name = "When have string like abc it should return null",
+        name = "When month is string it should return null",
         result = financialTracker.getTransactionByMonth("abc") == null,
         expectedResult = true
     )
+
     check(
-        name = "When have character like A it should return null",
-        result = financialTracker.getTransactionByMonth("A") == null,
-        expectedResult = true
-    )
-    check(
-        name = "When have empty String it should return null",
+        name = "When month is empty it should return null",
         result = financialTracker.getTransactionByMonth("") == null,
         expectedResult = true
     )
+
     check(
-        name = "When have special char like , it should return null",
-        result = financialTracker.getTransactionByMonth(",") == null,
-        expectedResult = true
-    )
-    check(
-        name = "When have space between number like 1 1 it should return null",
+        name = "When month contains space between it should return null",
         result = financialTracker.getTransactionByMonth("1 1") == null,
         expectedResult = true
     )
+
     check(
-        name = "When have floating or double number like 1.0 it should return null",
+        name = "When month is floating or double it should return null",
         result = financialTracker.getTransactionByMonth("1.0") == null,
         expectedResult = true
     )
     check(
-        name = "When have leading 0 with number two digit 011 it should return null",
+        name = "When month starts with leading 0 it should return null",
         result = financialTracker.getTransactionByMonth("011") == null,
         expectedResult = true
     )
     check(
-        name = "When month is not exist in memory {1,2,3} it should return null",
+        name = "When month is not exist it should return null",
         result = financialTracker.getTransactionByMonth("12") == null,
-        expectedResult = true
-    )
-    check(
-        name = "When memory is empty it should return null",
-        result = financialTracker.getTransactionByMonth("1") == null,
-        expectedResult = true
-    )
-    check(
-        name = "When input is number with characters like 1a it should return null",
-        result = financialTracker.getTransactionByMonth("1a") == null,
-        expectedResult = true
-    )
-    check(
-        name = "When input is number with special char like 1@ it should return null",
-        result = financialTracker.getTransactionByMonth("1@") == null,
-        expectedResult = true
-    )
-    check(
-        name = "When input is Arabic digit like ١ it should return null",
-        result = financialTracker.getTransactionByMonth("١") == null,
         expectedResult = true
     )
 
     /** Valid test cases */
     check(
-        name = "When we have number like 1 and exists in memory {1, 6, 8} it should return transaction list not empty",
+        name = "when month is exist for one time it should return value",
         result = financialTracker.getTransactionByMonth("1")?.isNotEmpty() == true,
         expectedResult = true
     )
     check(
-        name = "When we have number like 1 and exists in memory 3 times {1, 1, 1, 6, 8} it should return transaction list of 3 length",
+        name = "when month is exist for 3 times it should return value",
         result = financialTracker.getTransactionByMonth("1")?.size == 3,
-        expectedResult = true
-    )
-    check(
-        name = "When we have number like 03 and exists in memory {1, 3, 6, 8} it should return transaction list not empty",
-        result = financialTracker.getTransactionByMonth("03")?.isNotEmpty() == true,
         expectedResult = true
     )
 
     check(
-        name = "When input contains spaces left or right like (  2  ) and exists in memory {2, 3, 6, 8} it should return transaction list not empty",
-        result = financialTracker.getTransactionByMonth("2")?.isNotEmpty() == true,
+        name = "when month contains spaces around it should return value",
+        result = financialTracker.getTransactionByMonth(" 2 ")?.isNotEmpty() == true,
+        expectedResult = true
+    )
+    check(
+        name = "when month is arabic digits it should return value",
+        result = financialTracker.getTransactionByMonth("١")?.isNotEmpty() == true,
         expectedResult = true
     )
 }

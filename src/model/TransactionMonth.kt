fc387a4b3101c02739afec36241174407cd70b1e
Thoraya Hamdy <thoriahamdy99@gmail.com>
2025-04-11 01:23:03 +0200

    - Handle structure of transaction month to have order ID.

--- src/model/TransactionMonth.kt
+++ src/model/TransactionMonth.kt
@@ -1,16 +1,35 @@
 package src.model
 
-enum class TransactionMonth {
-    JANUARY,
-    FEBRUARY,
-    MARCH,
-    APRIL,
-    MAY,
-    JUNE,
-    JULY,
-    AUGUST,
-    SEPTEMBER,
-    OCTOBER,
-    NOVEMBER,
-    DECEMBER;
+enum class TransactionMonth(val orderId: Int) {
+    JANUARY(1),
+    FEBRUARY(2),
+    MARCH(3),
+    APRIL(4),
+    MAY(5),
+    JUNE(6),
+    JULY(7),
+    AUGUST(8),
+    SEPTEMBER(9),
+    OCTOBER(10),
+    NOVEMBER(11),
+    DECEMBER(12);
+
+    companion object {
+
+        fun getTransactionMonth(transactionMonthName: String?): TransactionMonth? {
+            if (transactionMonthName == null) return null
+            entries.forEach { transactionMonth ->
+                if (transactionMonth.name == transactionMonthName) return transactionMonth
+            }
+            return null
+        }
+
+        fun getTransactionMonth(transactionMonthOrderId: Int?): TransactionMonth? {
+            if (transactionMonthOrderId == null) return null
+            entries.forEach { transactionMonth ->
+                if (transactionMonth.orderId == transactionMonthOrderId) return transactionMonth
+            }
+            return null
+        }
+    }
 }

27c29ed39c9cee91421061692025b2080caf49ab
Mona <monaayman584@gmail.com>
2025-04-10 22:32:44 +0200

    add function to read month from user and get report for this month

--- src/report/handler/TransactionReportActionHandler.kt
+++ src/report/handler/TransactionReportActionHandler.kt
@@ -1,10 +1,35 @@
 package src.report.handler
 
 import src.common.console.handler.ActionHandler
+import src.model.Transaction
+import src.report.validation.TransactionReportActionValidator
 import src.storage.IFinancialTrackerStorage
 
 class TransactionReportActionHandler : ActionHandler {
-    override fun handleAction(financialTrackerRepository: IFinancialTrackerStorage) {
-        TODO("Not yet implemented")
+    override fun handleAction(financialTrackerStorage: IFinancialTrackerStorage) {
+        println("Please Enter Month to view Monthly Report : ")
+        val month = readlnOrNull()
+        val isValidMonth = TransactionReportActionValidator().isMonthValid(month)
+        if (!isValidMonth) {
+            println("Invalid Month!")
+            return
+        }
+        val transactionList: List<Transaction>? =
+            month?.toInt()?.let { financialTrackerStorage.getTransactionsByMonth(it) }
+        if (transactionList == null) {
+            println("No Transaction found !")
+            return
+        }
+        viewMonthlyReport(transactionList)
+
+
     }
+
+    private fun viewMonthlyReport(transactionList: List<Transaction>) {
+        transactionList.forEach { transaction ->
+            println("id = ${transaction.id},amount = ${transaction.amount}, day = ${transaction.date.day}, month = ${transaction.date.month}, year = ${transaction.date.year}, type = ${transaction.type}, category = ${transaction.category}")
+        }
+    }
+
+
 }

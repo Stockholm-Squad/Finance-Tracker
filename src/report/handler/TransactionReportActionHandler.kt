dc5a2dcd8d881ce0118fde30df271ebca2a97117
Thoraya Hamdy <thoriahamdy99@gmail.com>
2025-04-11 02:16:10 +0200

    - update print message and use data handler to get the monthly report.

--- src/report/handler/TransactionReportActionHandler.kt
+++ src/report/handler/TransactionReportActionHandler.kt
@@ -2,24 +2,39 @@ package src.report.handler
 
 import src.common.console.handler.ActionHandler
 import src.model.Transaction
+import src.model.TransactionMonth
+import src.report.validation.ITransactionReportActionValidator
 import src.report.validation.TransactionReportActionValidator
 import src.storage.IFinancialTrackerStorage
 
 class TransactionReportActionHandler : ActionHandler {
+
+    private val transactionReportActionValidator: ITransactionReportActionValidator = TransactionReportActionValidator()
+    private val transactionReportDataHandler: ITransactionReportDataHandler = TransactionReportDataHandler()
+
     override fun handleAction(financialTrackerStorage: IFinancialTrackerStorage) {
         println("Please Enter Month to view Monthly Report : ")
+        println("like ${TransactionMonth.JANUARY.orderId}.${TransactionMonth.JANUARY.name}" +
+                ", ${TransactionMonth.FEBRUARY.orderId}.${TransactionMonth.FEBRUARY.name}"+
+                ", ${TransactionMonth.MARCH.orderId}.${TransactionMonth.MARCH.name}"+
+                ", ${TransactionMonth.APRIL.name}.${TransactionMonth.APRIL.name}"
+        )
         val month = readlnOrNull()
-        val isValidMonth = TransactionReportActionValidator().isMonthValid(month)
+
+        val isValidMonth = transactionReportActionValidator.isMonthValid(month)
         if (!isValidMonth) {
             println("Invalid Month!")
             return
         }
+
         val transactionList: List<Transaction>? =
-            month?.toInt()?.let { financialTrackerStorage.getTransactionsByMonth(it) }
+            transactionReportDataHandler.getMonthlyReport(month, financialTrackerStorage)
+
         if (transactionList == null) {
-            println("No Transaction found !")
+            println("No Transaction found!")
             return
         }
+
         viewMonthlyReport(transactionList)
 
 

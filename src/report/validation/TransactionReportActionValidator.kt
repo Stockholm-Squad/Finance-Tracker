3c0389e758da7677150b1231afbaf2c483635dc9
Thoraya Hamdy <thoriahamdy99@gmail.com>
2025-04-11 02:15:05 +0200

    - handle validator function to validate based on TransactionMonth.

--- src/report/validation/TransactionReportActionValidator.kt
+++ src/report/validation/TransactionReportActionValidator.kt
@@ -1,16 +1,23 @@
 package src.report.validation
 
+import src.model.TransactionMonth
+
 
 class TransactionReportActionValidator : ITransactionReportActionValidator {
-    override fun isMonthValid(month: String?): Boolean {
 
+    override fun isMonthValid(month: String?): Boolean {
         val trimMonth = month?.trim() ?: return false
         if (trimMonth.isEmpty()) return false
         if (trimMonth.first() == '0') return false
-        val isDigit = trimMonth.all { it.isDigit() }
-        if (!isDigit) return false
-        val monthAsDigit = trimMonth.toInt()
-        return !(monthAsDigit < 1 || monthAsDigit > 12)
+        return isTransactionMonth(trimMonth)
+    }
 
+    private fun isTransactionMonth(trimMonth: String): Boolean {
+        val isDigit = trimMonth.all { it.isDigit() }
+        if (isDigit) {
+            val monthAsInt = trimMonth.toInt()
+            if (TransactionMonth.getTransactionMonth(monthAsInt) != null) return true
+        }
+        return TransactionMonth.getTransactionMonth(trimMonth) != null
     }
 }

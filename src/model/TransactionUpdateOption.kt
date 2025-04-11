6df471e7b72601288ef2102b90cac67a40d0da02
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-04-11 19:23:11 +0200

    Re_Arrange options at TransactionUpdateOption

--- src/model/TransactionUpdateOption.kt
+++ src/model/TransactionUpdateOption.kt
@@ -1,8 +1,8 @@
 package src.model
 
 enum class TransactionUpdateOption(val option: Int) {
-    TYPE(1),
-    CATEGORY(2),
+    CATEGORY(1),
+    TYPE(2),
     AMOUNT(3),
     DATE(4),
     EXIT(5);

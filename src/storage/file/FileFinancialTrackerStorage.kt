77f5ba515f3e847f9a3b896eaa4e929bfb60292e
Thoraya Hamdy <thoriahamdy99@gmail.com>
2025-04-12 16:27:16 +0200

    - create file financial tracker storage to deal with the transaction operations using file.

---
+++ src/storage/file/FileFinancialTrackerStorage.kt
@@ -0,0 +1,69 @@
+package src.test.storage.file
+
+import src.model.Transaction
+import src.model.TransactionMonth
+import src.storage.IFinancialTrackerStorage
+
+class FileFinancialTrackerStorage(
+    private val fileHelper: IFileHelper
+) : IFinancialTrackerStorage {
+    companion object {
+        var allTransaction: MutableList<Transaction>? = mutableListOf()
+    }
+
+    init {
+        allTransaction = fileHelper.loadTransactions()
+    }
+
+    override fun addTransaction(transaction: Transaction): Boolean {
+        allTransaction?.add(transaction)
+        try {
+            fileHelper.saveTransactions(allTransaction?.toList())
+            return true
+        } catch (throwable: Throwable) {
+            return false
+        }
+    }
+
+    override fun deleteTransactionById(transactionId: String): Boolean {
+        val transactionRemoved = allTransaction?.removeIf { it.id == transactionId } ?: false
+        if (!transactionRemoved) return false
+        try {
+            fileHelper.saveTransactions(allTransaction?.toList())
+            return true
+        } catch (throwable: Throwable) {
+            return false
+        }
+    }
+
+    override fun updateTransaction(transaction: Transaction): Boolean {
+        val index = allTransaction?.indexOfFirst { it.id == transaction.id } ?: return false
+        if (index == -1) return false
+
+        allTransaction?.set(index, transaction) ?: return false
+        try {
+            fileHelper.saveTransactions(allTransaction?.toList())
+            return true
+        } catch (throwable: Throwable) {
+            return false
+        }
+    }
+
+    override fun getAllTransactions(): List<Transaction> {
+        return fileHelper.loadTransactions() ?: listOf()
+    }
+
+    override fun getTransactionById(transactionId: String): Transaction? {
+        return allTransaction?.find { it.id == transactionId }
+    }
+
+    override fun getTransactionsByMonth(transactionMonth: TransactionMonth): List<Transaction> {
+        val result: MutableList<Transaction> = mutableListOf()
+        allTransaction?.forEach { transaction ->
+            if (transaction.date.month == transactionMonth)
+                result.add(transaction)
+        }
+        return result
+    }
+
+}
